3b0d6693305ca54a49bb5f36ba11e0a0def9e75a
Anthony Lee Zhang <anthonyzhang0@gmail.com>
2025-12-05 08:42:15 -0600

    First commit

---
+++ cryptoexchange_mathematica/polynomial_rootfind.R
@@ -0,0 +1,3 @@
+
+
+uniroot(function(r) {return(r^3 - r * 4 - 2)}, lower = 0, upper = 10, tol = .Machine$double.eps)
---
+++ cryptoexchange_mathematica/twonode_simulation.R
@@ -0,0 +1,14 @@
+
+gamma = 1
+zeta = 1
+
+x1 = 2
+x2 = -1
+
+z12 = (gamma / (2 * gamma + zeta)) * (x1 - x2)
+
+inv1 = x1 - z12
+inv2 = x2 + z12
+
+p1 = -gamma * inv1
+p2 = -gamma * inv2
